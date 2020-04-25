5e081f85d4ec01dd7ed64fafbd81ae4d74a21b0d
Sanjeev Sreenath <sreenath.sa@husky.neu.edu>
2020-04-25 16:07:54 -0400

    refactored to reduce LOC

--- static/randomName.js
+++ static/randomName.js
@@ -6,23 +6,20 @@ $(".case-button").on('click', function() {
   var tooltip = $(this);
   tooltip.attr('data-original-title', 'Copied!');
   tooltip.tooltip('show');
-});
-
-$(".case-button").on('mouseleave', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copy Text');
-});
-
-function copy(text) {
-    var input = document.createElement('input');
+  text = tooltip.attr('name');
+  var input = document.createElement('input');
     input.setAttribute('value', text);
     document.body.appendChild(input);
     input.select();
     var result = document.execCommand('copy');
     document.body.removeChild(input);
     return result;
- }
+});
 
+$(".case-button").on('mouseleave', function() {
+  var tooltip = $(this);
+  tooltip.attr('data-original-title', 'Copy Text');
+});
 
 function anotherName() {
     location.reload();
