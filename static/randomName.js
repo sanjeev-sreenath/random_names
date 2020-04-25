482ab0ebe27256a57eb9ab1ed0e94c1bb137c338
Sanjeev Sreenath <sreenath.sa@husky.neu.edu>
2020-04-25 15:56:28 -0400

    done with tooltips

--- static/randomName.js
+++ static/randomName.js
@@ -2,58 +2,15 @@ $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-// $("a.my-tool-tip").tooltip();
-// $("#copy-button").on('click', function() {
-//   var tooltip = $(this);
-//   tooltip.attr('data-original-title', 'Copied!');
-//   $("a.my-tool-tip").tooltip('show');
-// });
+$(".case-button").on('click', function() {
+  var tooltip = $(this);
+  tooltip.attr('data-original-title', 'Copied!');
+  tooltip.tooltip('show');
+});
 
-// function sample(abcd) {
-//     var tooltip = document.getElementById('my-button2');
-//     tooltip.attr('data-original-title', 'WOW!');
-//     $("#my-button2").tooltip('show');
-// }
-// $("#my-button2").on('click', function() {
-//   var tooltip = $(this);
-//   tooltip.attr('data-original-title', 'Copied!');
-//   $("#my-button1").tooltip('show');
-// });
-
-$("#0").on('click', function() {
+$(".case-button").on('mouseleave', function() {
   var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#0").tooltip('show');
-});
-$("#1").on('click', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#1").tooltip('show');
-});
-$("#2").on('click', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#2").tooltip('show');
-});
-$("#3").on('click', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#3").tooltip('show');
-});
-$("#4").on('click', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#4").tooltip('show');
-});
-$("#5").on('click', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#5").tooltip('show');
-});
-$("#6").on('click', function() {
-  var tooltip = $(this);
-  tooltip.attr('data-original-title', 'Copied!');
-  $("#6").tooltip('show');
+  tooltip.attr('data-original-title', 'Copy Text');
 });
 
 function copy(text) {
